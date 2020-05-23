d0014d1cbf8d672effff80fd25b2ac57ff9f4fef
Rahulchandnani1 <64478378+Rahulchandnani1@users.noreply.github.com>
2020-05-23 22:39:22 +0530

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import smtplib
+import urllib.request as urllib
+# Senders email
+sender_email = "tnamaste370@gmail.com"
+# Receivers email
+receiveremail = "tnamaste573@gmail.com"
+message ="Best Model is being created... Thank You"
+# Initialize the server variable
+server = smtplib.SMTP('smtp.gmail.com', 587)
+
+server.starttls()
+
+server.login("tnamaste370@gmail.com", "")
+print("Login Success!")
+
+server.sendmail("Rahul chandnani", "tnamaste573@gmail.com", message)
+print(f"Email has been sent successfully to {rec_email}")
+
+
+# In[ ]:
+
+
+
+
